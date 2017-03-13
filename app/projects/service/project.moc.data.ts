f432d79b249c2de062c804629c80e6a0369d60af
Rajesh Kumar Jha <rkjha.it.in@gmail.com>
2017-03-13 13:26:30 +0530

    Update project.moc.data.ts

--- app/projects/service/project.moc.data.ts
+++ app/projects/service/project.moc.data.ts
@@ -9,7 +9,9 @@ export const PROJECT: ProjectDetails = {
                         "image": "assets/images/portfolio/urbantouch_logo.png",
                         "title": "CIAP (Citibank Infrastructure Application Portal)",
                         "url": "./",
-                        "description": "The purpose of CIAP Web Project is to create a Single Page Application to provide Interface to manage Linux/Windows server administration for internal use."
+                        "description": ["Admin Console is a part of the overall Commerce Platform solution which is a digital platform to bring the WellsFargo wholesale customers and their customers onto a single platform to enhance their business using the services like loyalty programs, reservations, campaigns, location services etc. being offered through this platform.",
+"The Admin Console will facilitate the WellsFargo Admin to onboard and manage Companies, Facilities, Merchants, APPs, Campaigns, Reservations, Related Messages, Configurations and Consumers. It will support single sign-on authentication and role based authorization. WF Admin will be able to create & manage users having different roles such as Company Admin, Facility Admin & Staff Admin with different authorizations to perform their functions"]
+
                   }]
             },
             {
@@ -18,37 +20,37 @@ export const PROJECT: ProjectDetails = {
                         "image": "assets/images/portfolio/kuoni_logo.png",
                         "title": "Title",
                         "url": "./",
-                        "description": "Description"
+                  "description": ["Description"]
                   }, {
                         "image": "assets/images/portfolio/jato_logo.png",
                         "title": "Title",
                         "url": "./",
-                        "description": "Description"
+                        "description": ["Description"]
                   }, {
                         "image": "assets/images/portfolio/staples_logo.png",
                         "title": "Title",
                         "url": "./",
-                        "description": "Description"
+                        "description": ["Description"]
                   }, {
                         "image": "assets/images/portfolio/coach_logo.png",
                         "title": "Title",
                         "url": "./",
-                        "description": "Description"
+                        "description": ["Description"]
                   }, {
                         "image": "assets/images/portfolio/diesel_logo.png",
                         "title": "Title",
                         "url": "./",
-                        "description": "Description"
+                        "description": ["Description"]
                   }, {
                         "image": "assets/images/portfolio/johndeere_logo.png",
                         "title": "Title",
                         "url": "./",
-                        "description": "Description"
+                        "description": ["Description"]
                   }, {
                         "image": "assets/images/portfolio/tiffany_logo.png",
                         "title": "Title",
                         "url": "./",
-                        "description": "Description"
+                        "description": ["Description"]
                   }]
             },
             {
@@ -57,7 +59,7 @@ export const PROJECT: ProjectDetails = {
                         "image": "assets/images/portfolio/urbantouch_logo.png",
                         "title": "Title",
                         "url": "./",
-                        "description": "Description"
+                        "description": ["Description"]
                   }]
             }, {
                   "name": "componence",
@@ -65,17 +67,17 @@ export const PROJECT: ProjectDetails = {
                         "image": "assets/images/portfolio/papergoods_logo.png",
                         "title": "Title",
                         "url": "./",
-                        "description": "Description"
+                        "description": ["Description"]
                   }, {
                         "image": "assets/images/portfolio/yaysave_logo.png",
                         "title": "Title",
                         "url": "./",
-                        "description": "Description"
+                        "description": ["Description"]
                   }, {
                         "image": "assets/images/portfolio/watterbobble_logo.png",
                         "title": "Title",
                         "url": "./",
-                        "description": "Description"
+                        "description": ["Description"]
                   }]
             }]
 
