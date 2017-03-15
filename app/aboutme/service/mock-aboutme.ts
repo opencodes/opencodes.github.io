a89535cd9f9af6fa09d05d2799a7e7e572001f39
Rajesh Kumar Jha <rkjha.it.in@gmail.com>
2017-03-15 16:08:23 +0530

    Create mock-aboutme.ts

---
+++ app/aboutme/service/mock-aboutme.ts
@@ -0,0 +1,29 @@
+import {Aboutme} from './profile';
+
+export const ABOUTME  = {"name":{
+         "firstname":"Rajesh",
+         "middlename":"Kumar",
+         "lastname":"Jha"
+      },
+      "email":"rkjha.it.in@gmail.com",
+      "phone":"7022368966",
+      "image": "assets/images/profile.jpg",                 
+      "coverletter":"Since 6+ year accomplished web design & front-end development with extensive experience building E-Commerce driven websites using open source and enterprise technologies, HTML5, CSS3 and various web scripting technologies, web standards and project management. Provide a diverse range of services including custom theme/application/plugin development and branding to an array of clients from a variety of industries. Enthusiastic web professional motivated by challenging projects and deadlines.",
+      "social" : [{
+            "title" : "facebook",
+            "url" : "https://www.facebook.com/rkjha.it.in"
+         },
+         {
+            "title" : "twitter",
+            "url" : "https://twitter.com/itsrkjha"
+         },
+         {
+            "title" : "linkedin",
+            "url" : "https://in.linkedin.com/in/rkjha1984"
+         },
+         {
+            "title" : "github",
+            "url" : "https://github.com/opencodes"
+         }
+      ]
+  }
