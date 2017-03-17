de8a07fbd208ac32ad6776516a3c53a056e375de
Rajesh Kumar Jha <rkjha.it.in@gmail.com>
2017-03-17 12:33:43 +0530

    Update aboutme.service.ts

--- app/aboutme/service/aboutme.service.ts
+++ app/aboutme/service/aboutme.service.ts
@@ -1,11 +1,42 @@
 import { Injectable } from '@angular/core';
 import {Aboutme} from './aboutme';
+import {Post} from './post';
 import {ABOUTME} from './mock-aboutme';
+import { Http, Response }          from '@angular/http';
+import 'rxjs/add/operator/toPromise';
+
+
 
 @Injectable()
 export class AboutmeService{
-    getProfile(){
-        return Promise.resolve(ABOUTME);
 
+     private heroesUrl = 'https://jsonplaceholder.typicode.com/posts';
+
+     constructor (private http: Http) {}
+
+     getPosts (): Promise<Post[]> {
+        return this.http.get(this.heroesUrl)
+                        .toPromise()
+                        .then(this.extractData)
+                        .catch(this.handleError);
+      }
+    private extractData(res: Response) {
+    let body = res.json();
+    return body.data || { };
+  }
+
+  private handleError (error: Response | any) {
+    // In a real world app, we might use a remote logging infrastructure
+    let errMsg: string;
+    if (error instanceof Response) {
+      const body = error.json() || '';
+      const err = body.error || JSON.stringify(body);
+      errMsg = `${error.status} - ${error.statusText || ''} ${err}`;
+    } else {
+      errMsg = error.message ? error.message : error.toString();
     }
+    console.error(errMsg);
+    return Promise.reject(errMsg);
+  }
+    
 }
