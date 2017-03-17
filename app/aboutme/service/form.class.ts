01cfb47416968ed4b3bfe4d8b317e4f609a0fdf9
Rajesh Kumar Jha <rkjha.it.in@gmail.com>
2017-03-17 12:35:29 +0530

    Create form.class.ts

---
+++ app/aboutme/service/form.class.ts
@@ -0,0 +1,9 @@
+export class FormClass{
+     constructor(
+        public id: number,
+        public name: string,
+        public power: string,
+        public alterEgo?: string
+      ) {  }
+
+}
